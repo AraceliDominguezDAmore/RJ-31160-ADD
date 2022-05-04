0cf65a2787a1c6179e2e4306807ca7f0019b3c51
AraceliDominguezDAmore <dominguezaraceli2701@gmail.com>
2022-05-04 16:48:06 -0300

    Primera Entrega Final - Clase 7

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import NavBar from './componentes/NavBar';
 import ItemListContainer from './componentes/ItemListContainer';
 import ItemDetailContainer from './componentes/ItemDetailContainer';
+import { BrowserRouter, Route, Routes} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <NavBar/> <br/>
-      <h1>Palo Macrameando</h1> <br/>
-      {/* <ItemListContainer titulo= "Lista de Productos"/> <br/> */}
-      <ItemDetailContainer/>
+      <BrowserRouter>
+        <NavBar/> 
+        <Routes>
+          <Route path= "/" element= { <ItemListContainer titulo="Bienvenido a nuestra tienda" /> } />
+          <Route path= "/category/:categoryid" element=  { <ItemListContainer titulo="Categorias de productos"/> } />
+          <Route path= "/discos/:itemid" element= {<ItemDetailContainer id= {3} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/componentes/NavBar.js
+++ src/componentes/NavBar.js
@@ -1,57 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import CartWidget from './CartWidget'
 
-const NavBar = () => {
+function NavBar () {
     return(
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabindex="0" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Galeria</a></li>
-                        <li tabindex="0">
-                            <a className="justify-between">
-                                Productos
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-                            </a>
-                            <ul className="p-2">
-                                <li><a>Anillos</a></li>
-                                <li><a>Aros</a></li>
-                                <li><a>Maceteros</a></li>
-                                <li><a>Pulsera</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Sucursales</a></li>
-                    </ul>
-                </div>
-                <a className="btn btn-ghost normal-case text-xl">Palo Macrameando</a>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal p-0">
-                    <li><a>Galeria</a></li>
-                    <li tabindex="0">
-                        <a>
-                            Productos
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-                        </a>
-                        <ul className="p-2">
-                            <li><a>Anillos</a></li>
-                            <li><a>Aros</a></li>
-                            <li><a>Maceteros</a></li>
-                            <li><a>Pulsera</a></li>
-                        </ul>
+        <header className="bg-indigo-300 container-fluid shadow-xl text-gray-600 body-font mb-5">
+            <nav className="flex justify-between">
+                <h2>Disqueria Online</h2>
+                <ul>
+                    <li>
+                        <Link to="/category/One Direction" className="mr-5 hover:text-gray-900">1 D</Link>
+                        <Link to="/category/5 Seconds of Summer" className="mr-5 hover:text-gray-900">5 SOS</Link>
+                        <Link to="/category/BTS" className="mr-5 hover:text-gray-900">BTS</Link>
                     </li>
-                    <li><a>Sucursales</a></li>
-                </ul>
-            </div>
-            
-            <div className="navbar-end">
-                <CartWidget/>
-                <a className="btn btn-accent btn-outline">Comprar</a>
-            </div>
-        </div>
+                </ul>  
+                <div>
+                    <CartWidget/>
+                </div>
+            </nav>
+        </header>
     )
 }
 
